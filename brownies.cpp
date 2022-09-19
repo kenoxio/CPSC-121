e090dc3e53ec3bee6d003eccf7535772d800d8ce
kenoxio <nickbillnicole@protonmail.com>
2022-09-18 19:37:34 -0700

    homework02

--- brownies.cpp
+++ brownies.cpp
@@ -20,6 +20,24 @@ int main(){
     // cout << totalcookiesUSD << std::endl;
     double totalbrowniesUSD = fundraisinggoalUSD / browniesUSD;
     // cout << totalbrowniesUSD << std::endl;
-    cout << "To make our fundraising goal of $300, we would need to sell "  << totalcookiesUSD << " of Cookies at $2.50" << endl;
-    cout << "To make our fundraising goal of $300, we would need to sell " << totalbrowniesUSD << " of Brownies at $3.75" << endl;
+    cout << "To make our fundraising goal of $" << fundraisinggoalUSD << ", we would need to sell "  << totalcookiesUSD << " of Cookies at $2.50" << endl;
+    cout << "To make our fundraising goal of $" << fundraisinggoalUSD << ", we would need to sell " << totalbrowniesUSD << " of Brownies at $3.75" << endl;
+
+    cout << "**************************************************************************************" << endl;
+    double cookiessoldUSD = 30;
+    double neededfundsUSD = fundraisinggoalUSD - (cookiessoldUSD * cookiesUSD);
+    double browniesneededUSD = neededfundsUSD / browniesUSD;
+    cout << "To make our fundraising goal of " << fundraisinggoalUSD << ", when we already sold " << cookiessoldUSD << " cookies at $" << cookiesUSD << ", we would need to sell " << browniesneededUSD << " brownies at $" << browniesUSD << "." << endl;
+    cout << "**************************************************************************************" << endl;
+
+    double cookiesingredientUSD = 0.10;
+    double browniesingredientUSD = 0.17;
+    double cookiessoldUSD2 = 43;
+    double browniessoldUSD = 63; 
+    double profitcookies = (cookiesUSD * cookiessoldUSD2) - (cookiesingredientUSD * cookiessoldUSD2);
+    double profitbrownies = (browniesUSD * browniessoldUSD) - (browniesingredientUSD * browniessoldUSD);
+    double totalprofit = profitbrownies + profitcookies;
+
+    cout << "We made a total of $" << totalprofit << " by selling " << cookiessoldUSD2 << " cookies at $" << cookiesUSD << "0 and " << browniesneededUSD << " brownies at $" << browniesUSD << " when ingredients cost $" << cookiesingredientUSD << "0 for cookies and $" << browniesingredientUSD << " for brownies." << endl;
+
 }
