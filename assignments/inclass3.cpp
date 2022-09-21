59f365e3553430193041cf67ccf122604155ec5c
kenoxio <nickbillnicole@protonmail.com>
2022-09-21 12:06:00 -0700

    ascii art

---
+++ assignments/inclass3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+char slash = 47;
+char slash2 = 92;
+char par = 40;
+char par2 = 41; 
+cout << " " << slash << slash2 << " " << slash << slash2 << endl;
+cout << par << par << "ovo" << par2 << par2 << endl;
+cout << par << par2 << ":::" << par << par2 << endl;
+cout << "  " << "VVV" << endl;
+}
+
+/*  /\ /\ 
+   ((ovo))
+   ():::()
+     VVV
+ */
