8c084c3e42d051a006d871c2a9f61a2600fb6ea1
kenoxio <nickbillnicole@protonmail.com>
2022-09-21 11:35:44 -0700

    inclass assignment

---
+++ inclass.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+main(){
+    int x; 
+    int y;
+    cout << "*****************************************************" << endl;
+    cout << "Type your first number: "; // Numerator
+    cin >> x; // User Input
+    cout << "Now type in your second number: "; // Denominator
+    cin >> y; // User Input
+    cout << "*****************************************************" << endl;
+    cout << "1st Number: " << y << "\n" << "2nd Number: " << x << endl;
+    cout << "*****************************************************" << endl;
+    cout << "The result of the 1st Number / 2nd Number as a whole number is " << x / y  << "." << '\n' << "The remainder is " << x % y << "." << endl;
+    cout << "*****************************************************" << endl;
+    cout << "The result of the 1st Number * 2nd Number is " << x * y << "." << endl;
+}
